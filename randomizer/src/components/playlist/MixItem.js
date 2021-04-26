6ea7bb414cfc87bfbd2d4deaa39613966d5ffe55
Matt Harris <711mattman@gmail.com>
2021-04-26 14:49:22 -0400

    Got the youtube videos popping up

--- randomizer/src/components/playlist/MixItem.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SimplePlaylistItem from './SimplePlaylistItem';
-
-const MixItem = ({ name, onClick, onDelete }) => (
-  <SimplePlaylistItem onClick={onClick} id={name} title={name} onDelete={onDelete} />
-);
-
-export default React.memo(MixItem);
